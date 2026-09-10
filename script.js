74e4289c85e68702bd0e66281c7e2ad3ceb8dc4b
sy6685216-wq <sy6685216@gmail.com>
2026-09-10 21:24:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelectorAll('button').forEach(button=>{
+  button.addEventListener('click',()=>alert('Selling/downloads will be added later. This first version is only a working preview.'));
+});
